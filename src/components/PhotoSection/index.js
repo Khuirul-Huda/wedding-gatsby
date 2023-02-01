a19b249c7f2ab40c31796ed21961d7a69c0b4bde
Khuirul Huda <67778682+Khuirul-Huda@users.noreply.github.com>
2023-02-01 08:20:07 +0700

    update link

--- src/components/PhotoSection/index.js
+++ src/components/PhotoSection/index.js
@@ -6,15 +6,15 @@ import { photos } from './photo-data';
 function PhotoSection() {
   const renderYoutubeVideo = () => {
     return (
-      <!--<iframe
-        title="Pre-Wedding Dinda & Indra"
+      <iframe
+        title="Ceramah Singkat"
         width="100%"
         height="360px"
-        src="https://www.youtube.com/embed/75w39OY7N-k"
+        src="https://www.youtube.com/embed/_JidbICjlRw"
         frameBorder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
-      ></iframe>-->
+      ></iframe>
     );
   };
 
