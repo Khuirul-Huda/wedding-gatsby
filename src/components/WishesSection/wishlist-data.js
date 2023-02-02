4f0a827f3a3fb918ac8f7fe7af60d4992ac3bb37
Khuirul-Huda <hudah2102@gmail.com>
2023-02-02 21:55:58 +0700

    customize wishes

--- src/components/WishesSection/wishlist-data.js
+++ src/components/WishesSection/wishlist-data.js
@@ -1,17 +1,98 @@
-import Nita from '@assets/images/w-nita.jpg';
-import ihsan from './assets/w-ihsan.jpg';
+import Hanum from '@assets/images/0hanum.jpg';
+import Riski from '@assets/images/0riski.webp';
+import Reva from '@assets/images/0reva.jpg';
+import Dini from '@assets/images/0dini.jpg';
+import Lutfia from '@assets/images/0lutfia.jpg';
+import Aura from '@assets/images/0aura.jpg';
+import Nandha from '@assets/images/0nandha.jpg';
+import Hasna from '@assets/images/0hasna.jpg';
+import Ely from '@assets/images/0ely.jpg';
+import Qonita from '@assets/images/0qonita.jpg';
+import Feza from '@assets/images/0feza.jpg';
+import Nadya from '@assets/images/0nadya.jpg';
+import Ais from '@assets/images/0ais.jpg';
 
 export const wishlist = [
   {
-    name: 'Nita Syarifah',
-    infoName: 'Sahabat SMP Dinda',
-    description: `selamat menempuh hidup baru dinda dan mas indra semoga menjadi pasangan teman hidup selamanya sesurga, enjoy your little family always be happy my sweet couple!🥰🤗 amin`,
-    image: Nita,
+    name: 'Razan Hanum',
+    infoName: 'Sahabat SMA Luna',
+    description: `HWD ya luna daffa, samawaa teruss aamiin`,
+    image: Hanum,
   },
   {
-    name: 'Ihsan Satriawan',
-    infoName: 'Rekan Kerja Indra, Tokopedia',
-    description: `Selamat menggenapkan separuh Agama ya Indra dan Dinda. Semoga melalui pernikahan ini, kalian bisa saling menguatkan dalam mengarungi kehidupan ini baik dalam kondisi senang ataupun susah. Serta semakin dekat dan konsisten dalam menjalankan perintah Agama.`,
-    image: ihsan,
+    name: 'Riski Alyana',
+    infoName: 'Sahabat SMA Luna',
+    description: `HAPPY WEDDING LUNA DAN DAFFA💐 Hope u can be a happy familyyyy, doa terbaik untuk kalian berduaaaa! samawaa❤️`,
+    image: Riski,
   },
+  {
+    name: 'Revalia Fadhilla',
+    infoName: 'Sahabat SMA Luna',
+    description: `happy weeding luna dan daffa, semoga sakinah mawadah warohmah yaa 💓🤩`,
+    image: Reva,
+  },
+  {
+    name: 'Nurhardini Yulia Putri ',
+    infoName: 'Sahabat SMA Luna',
+    description: `Here's to a long and happy marriage! Wishing you all of the love and happiness!`,
+    image: Dini,
+  },
+  {
+    name: 'Lutfia Nias Maharani',
+    infoName: 'Sahabat SMA Luna',
+    description: `be happy with what you have lun`,
+    image: Lutfia,
+  },
+  {
+    name: 'Aura Btari Larasati Handoko',
+    infoName: 'Sahabat SMA Luna',
+    description: `HAPPY WEDDING DAFFA DAN LUNAAA semoga selalu menjadi pasutri yg samawa dan selalu diiringi kebahagiaan oleh Allah. aamiin🤍`,
+    image: Aura,
+  },
+  {
+    name: 'Aisyha Whinandha Putry',
+    infoName: 'Sahabat SMA Luna',
+    description: `"Halooo sayanggg
+    happy wedding Luna & Daffa, semoga sakinah mawadah warohmah till Jannah, bahagia dunia akhirat, lekas menjadi bapak dan ibu ya heheheh.
+    Emang jodoh ga ada yang tau doa terbaik untukmu sayanggg💗"`,
+    image: Nandha,
+  },
+  {
+    name: 'Hasna Zahra Agustin',
+    infoName: 'Sahabat SMA Luna',
+    description: `Happy Wedding Kak Luna & Kak Daffa. Semoga cepet diberi dedek bayi comel xixi`,
+    image: Hasna,
+  },
+  {
+    name: 'Ely Nurul Aini',
+    infoName: 'Sahabat SMA Luna',
+    description: `Happy wedding, semangat mendapat beban hidup baru`,
+    image: Ely,
+  },
+  {
+    name: "Qonita Zaimatul 'Ula ",
+    infoName: 'Sahabat SMA Luna',
+    description: `"Happy Wedding Daffa & Luna!!!
+    May both of you always be happy and always in God's grace."`,
+    image: Qonita,
+  },
+  {
+    name: 'fezaaa',
+    infoName: 'Sahabat SMA Luna',
+    description: `selamat menempuh hidup baru luna & daffa, semoga menjadi pasangan samawa `,
+    image: Feza,
+  },
+  {
+    name: 'Haya Asma Nadya',
+    infoName: 'Sahabat SMA Luna',
+    description: `Happy Wedding Daffa dan Luna. Semoga Samawa till janah yaa, semoga rezeki nya lancar, seneng & sedih bareng-bareng terus, dan semoga langgeng, amin. Selamat Luna akhirnya dapet jodoh yang real, sekian Heeseung punya Haya🙏🏻`,
+    image: Nadya,
+  },
+  {
+    name: 'Raden Roro Aisyah Nurrahma Andrika Putri',
+    infoName: 'Sahabat SMA Luna',
+    description: `Selamat menempuh lembaran baru untuk kalian berduaa! Semoga lembaran baru kalian akan selalu terisi oleh kebahagiaan dan keberkahan! `,
+    image: Ais,
+  },
+  
 ];
