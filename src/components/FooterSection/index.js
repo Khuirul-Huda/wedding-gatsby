22206c82f3122f07aa6a7b278d0351983054284a
Khuirul Huda <67778682+Khuirul-Huda@users.noreply.github.com>
2023-02-04 09:27:57 +0700

    update footer

--- src/components/FooterSection/index.js
+++ src/components/FooterSection/index.js
@@ -32,7 +32,7 @@ function FooterSection({ isInvitation }) {
           <div className="row copyright">
             <div className="col-md-12 text-center">
               <p>
-                <small className="block">Created with ❤️ by {' '}<a href='https://instagram.com/mipan.smanega'>MIPAN SMANEGA Wedding Organizer</a>. All Rights Reserved.</small>
+                <small className="block">Created with ❤️ by {' '}<a href='https://instagram.com/mipan.smanega'>MIPAN SMANEGA (XII MIPA 1)</a>. All Rights Reserved.</small>
                 <small className="block">
                   Covid-19 Icon by{' '}
                   <a href="https://www.flaticon.com/packs/covid-protection-measures-5" target="_blank" rel="noreferrer">
